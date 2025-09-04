037ef19b85f7dae244f7603a24ee6dd548ee86b9
Hevanafa <dhevansafa4@gmail.com>
2025-09-04 21:28:30 +0700

    reduced BMFont indentation to 2 spaces

--- src/BMFont.cpp
+++ src/BMFont.cpp
@@ -5,96 +5,96 @@
 #include "BMFont.hpp"
 
 bool fileExists(std::string filename) {
-	return std::filesystem::exists(filename);
+  return std::filesystem::exists(filename);
 }
 
 bool startsWith(std::string txt, std::string searchStr) {
-	return txt.rfind(searchStr) == 0;
+  return txt.rfind(searchStr) == 0;
 }
 
 std::vector<std::string> split(const std::string& text, const std::string& delimiter) {
-	std::vector<std::string> result;
-	std::string str = text;
-	int pos = 0;
+  std::vector<std::string> result;
+  std::string str = text;
+  int pos = 0;
 
-	if (text.empty()) return result;
+  if (text.empty()) return result;
 
-	str = text;
+  str = text;
 
-	while ((pos = str.find(delimiter)) != std::string::npos) {
-		auto token = str.substr(0, pos);
-		result.push_back(token);
-		str.erase(0, pos + delimiter.length());
-	}
+  while ((pos = str.find(delimiter)) != std::string::npos) {
+    auto token = str.substr(0, pos);
+    result.push_back(token);
+    str.erase(0, pos + delimiter.length());
+  }
 
-	result.push_back(str);
+  result.push_back(str);
 
-	return result;
+  return result;
 }
 
 std::string replaceAll(const std::string& source, const std::string& searchStr, const std::string& replaceStr) {
-	std::string result = source;
-	int pos = 0;
-	
-	if (searchStr.empty()) return result;
+  std::string result = source;
+  int pos = 0;
+  
+  if (searchStr.empty()) return result;
 
-	while ((pos = result.find(searchStr, pos)) != std::string::npos) {
-		result.replace(pos, searchStr.length(), replaceStr);
-		pos += replaceStr.length();
-	}
+  while ((pos = result.find(searchStr, pos)) != std::string::npos) {
+    result.replace(pos, searchStr.length(), replaceStr);
+    pos += replaceStr.length();
+  }
 
-	return result;
+  return result;
 }
 
 BMFont::BMFont(std::string filename) {
-	if (!fileExists(filename)) {
-		panicHalt("Couldn't find " + filename + "!");
-		return;
-	}
+  if (!fileExists(filename)) {
+    panicHalt("Couldn't find " + filename + "!");
+    return;
+  }
 
-	std::ifstream fontFile(filename);
-	std::string line;
-	std::vector<std::string> pairs, pair;
-	std::string k, v;
+  std::ifstream fontFile(filename);
+  std::string line;
+  std::vector<std::string> pairs, pair;
+  std::string k, v;
 
-	// TODO: Implement the loader
+  // TODO: Implement the loader
 
-	if (fontFile.is_open()) {
-		while (std::getline(fontFile, line)) {
-			pairs.clear();
+  if (fontFile.is_open()) {
+    while (std::getline(fontFile, line)) {
+      pairs.clear();
 
-			if (startsWith(line, "info")) {
-				// printf("%s\n", line.c_str());
-				pairs = split(line, " ");
+      if (startsWith(line, "info")) {
+        // printf("%s\n", line.c_str());
+        pairs = split(line, " ");
 
-				for (const auto& p: pairs) {
-					pair = split(p, "=");
-					
-					if (pair.size() == 2) {
-						k = pair.at(0); v = pair.at(1);
-						if (k == "face") {
-							face = replaceAll(v, "\"", "");
-							printf("Font face: %s\n", face.c_str());
-						}
-					}
-				}
+        for (const auto& p: pairs) {
+          pair = split(p, "=");
+          
+          if (pair.size() == 2) {
+            k = pair.at(0); v = pair.at(1);
+            if (k == "face") {
+              face = replaceAll(v, "\"", "");
+              printf("Font face: %s\n", face.c_str());
+            }
+          }
+        }
 
-				// TODO: Continue this
-			}
-		}
-	}
+        // TODO: Continue this
+      }
+    }
+  }
 }
 
 BMFont::~BMFont() {
-	for (const auto& g: glyphs)
-		delete g.second;
+  for (const auto& g: glyphs)
+    delete g.second;
 
-	glyphs.clear();
+  glyphs.clear();
 
-	delete imgData;
-	imgData = nullptr;
+  delete imgData;
+  imgData = nullptr;
 }
 
 int BMFont::getLineHeight() {
-	return lineHeight;
+  return lineHeight;
 }
