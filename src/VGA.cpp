def719b30d333a2c29a5c2516f88925056ef1c71
Hevanafa <dhevansafa4@gmail.com>
2025-09-05 07:04:09 +0700

    removed legacy TTF code

--- src/VGA.cpp
+++ src/VGA.cpp
@@ -24,18 +24,6 @@ void VGA::initSurface() {
 void VGA::initFont() {
   // defaultFont = new BMFont("assets\\fonts\\chikarego_regular_14.txt");
   defaultFont = new BMFont("assets\\fonts\\nokia_cellphone_fc_8.txt");
-
-  // if (TTF_Init() < 0) {
-  // 	printf("Couldn't init SDL_ttf! Reason: %s\n", TTF_GetError());
-  // 	return;
-  // }
-
-  // // Load game assets
-  // nokiaFont = TTF_OpenFont("assets\\fonts\\nokiafc22.ttf", 16);
-  // if (nokiaFont == nullptr) {
-  // 	printf("Couldn't load font! Reason: %s\n", TTF_GetError());
-  // 	return;
-  // }
 }
 
 int VGA::getScreenWidth() {
@@ -47,10 +35,7 @@ int VGA::getScreenHeight() {
 }
 
 int VGA::getLineHeight() {
-  return 0;
-  // if (nokiaFont == nullptr) return 0;
-
-  // return TTF_FontLineSkip(nokiaFont);
+  return defaultFont->getLineHeight();
 }
 
 void VGA::print(std::string text, int x, int y) {
@@ -62,16 +47,6 @@ void VGA::print(std::string text, int x, int y) {
   if (defaultFont == nullptr) return;
 
   printBMFont(*defaultFont, text, x, y, sdlSurface);
-
-  // if (nokiaFont == nullptr) return;
-
-  // TTF_SizeText(nokiaFont, text.c_str(), &w, &h);
-  // tempSurface = TTF_RenderText_Solid(nokiaFont, text.c_str(), colour);
-
-  // destRect = {x, y, w, h};
-  // SDL_BlitSurface(tempSurface, nullptr, sdlSurface, &destRect);
-
-  // SDL_FreeSurface(tempSurface);
 }
 
 void VGA::flush() {
@@ -80,8 +55,6 @@ void VGA::flush() {
 
 void VGA::freeFont() {
   delete defaultFont;
-  // TTF_CloseFont(nokiaFont);
-  // TTF_Quit();
 }
 
 void VGA::freeSurface() {
